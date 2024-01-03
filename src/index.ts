6e333643e5ef283d349670bfc245aa3ea581eb68
Guilherme Werner <guilherme.werner@tribufu.com>
2024-01-03 11:03:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 // Copyright (c) Tribufu. All Rights Reserved.
 
-import { TRIBUFU_VERSION, TRIBUFU_API_URL, TRIBUFU_CDN_URL } from "./constants";
-import { TribufuApi } from "./api";
-import { TribufuApiOptions } from "./options";
-import { TribufuBot } from "./bot";
-import { TribufuClient } from "./client";
-import { TribufuServer } from "./server";
-import { TribufuHttp, TribufuHttpOptions } from "./http";
+import {
+    TRIBUFU_VERSION,
+    TRIBUFU_API_URL,
+    TRIBUFU_CDN_URL
+} from "./constants";
 
 export {
     TRIBUFU_API_URL,
     TRIBUFU_CDN_URL,
     TRIBUFU_VERSION,
 };
+
+import { TribufuApi } from "./api";
+import { TribufuApiOptions } from "./options";
+import { TribufuBot } from "./bot";
+import { TribufuClient } from "./client";
+import { TribufuServer } from "./server";
+
+import {
+    TribufuHttp,
+    TribufuHttpOptions
+} from "./http";
 
 export {
     TribufuApi,
@@ -22,4 +31,34 @@ export {
     TribufuHttp,
     TribufuHttpOptions,
     TribufuServer,
+};
+
+import {
+    OAuth2AuthorizeRequest,
+    OAuth2ClientType,
+    OAuth2CodeResponse,
+    OAuth2GrantType,
+    OAuth2IntrospectionRequest,
+    OAuth2IntrospectionResponse,
+    OAuth2ResponseType,
+    OAuth2RevokeRequest,
+    OAuth2TokenHintType,
+    OAuth2TokenRequest,
+    OAuth2TokenResponse,
+    OAuth2TokenType
+} from "./oauth2";
+
+export {
+    OAuth2AuthorizeRequest,
+    OAuth2ClientType,
+    OAuth2CodeResponse,
+    OAuth2GrantType,
+    OAuth2IntrospectionRequest,
+    OAuth2IntrospectionResponse,
+    OAuth2ResponseType,
+    OAuth2RevokeRequest,
+    OAuth2TokenHintType,
+    OAuth2TokenRequest,
+    OAuth2TokenResponse,
+    OAuth2TokenType
 };
