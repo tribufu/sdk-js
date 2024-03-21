23c6f5b30ade1047fb98b8ee3829a7e93255fa81
Guilherme Werner <guilhermewerner@tribufu.com>
2024-03-21 12:28:47 -0300

    Revert "Merge mintaka lib"
    This reverts commit c6136b6e21bfcc418c36d1beace8ed296e004043.

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,9 @@
 // Copyright (c) Tribufu. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
-import { HttpClient } from "./http/client";
-import { HttpHeaders } from "./http/headers";
+import { HttpHeaders, HttpClient } from "@tribufu/mintaka";
 import { JavaScriptRuntime } from "./node";
-import { JwtDecoder } from "./jwt";
-import { JsonCasing } from "./json";
+import { JsonCasing, JwtDecoder } from "@tribufu/mintaka";
 import { TokenPayload } from "./token";
 import { TRIBUFU_API_URL, TRIBUFU_VERSION } from ".";
 import { TribufuApiOptions } from "./options";
--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,8 @@
 // Copyright (c) Tribufu. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
-import { HttpHeaders } from "./http/headers";
-import { HttpCookieMap } from "./http/cookies";
-import { OAuth2GrantType, OAuth2IntrospectionRequest, OAuth2IntrospectionResponse, OAuth2TokenRequest, OAuth2TokenResponse } from "./oauth2";
+import { HttpCookieMap, HttpHeaders } from "@tribufu/mintaka";
+import { OAuth2GrantType, OAuth2IntrospectionRequest, OAuth2IntrospectionResponse, OAuth2TokenRequest, OAuth2TokenResponse } from "@tribufu/mintaka";
 import { TribufuApi } from "./api";
 
 /**
