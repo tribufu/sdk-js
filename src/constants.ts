93d89995b51db21d50177eaeec472efeb19b1103
Guilherme Werner <guilhermewerner@tribufu.com>
2024-10-02 21:07:53 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -9,16 +9,21 @@ import packageJson from "../package.json";
 export const TRIBUFU_VERSION: string = packageJson.version;
 
 /**
- * The default Tribufu API URL.
- */
-export const TRIBUFU_API_URL: string = "https://api.tribufu.com";
-
-/**
  * The default Tribufu WEB URL.
  */
 export const TRIBUFU_WEB_URL: string = "https://www.tribufu.com";
 
 /**
+ * The default Tribufu API URL.
+ */
+export const TRIBUFU_API_URL: string = "https://api.tribufu.com";
+
+/**
  * The default Tribufu CDN URL.
  */
 export const TRIBUFU_CDN_URL: string = "https://cdn.tribufu.com";
+
+/**
+ * Tribufu copyright notice.
+ */
+export const TRIBUFU_COPYRIGHT = `© ${new Date().getFullYear()} Tribufu.`;
