1ab428cfd733d37b04dcaf3e53302edb0f09c130
Guilherme Werner <guilhermewerner@tribufu.com>
2024-10-18 13:16:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 // Copyright (c) Tribufu. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
+import "./api/api.base";
+import "./api/api.generated";
+import "./api/api.include";
+import "./api/index";
+import "./api/singletion";
+import "./http/headers";
+import "./node";
+
 import {
     TRIBUFU_API_URL,
     TRIBUFU_CDN_URL,
@@ -8,9 +16,16 @@ import {
     TRIBUFU_WEB_URL,
 } from "./constants";
 
-export { TRIBUFU_API_URL, TRIBUFU_CDN_URL, TRIBUFU_VERSION, TRIBUFU_WEB_URL };
-
 import { TribufuApi } from "./api";
 import { TribufuApiOptions } from "./options";
+import { TribufuApiSingleton } from "./api/singletion";
 
-export { TribufuApi, TribufuApiOptions };
+export {
+    TRIBUFU_API_URL,
+    TRIBUFU_CDN_URL,
+    TRIBUFU_VERSION,
+    TRIBUFU_WEB_URL,
+    TribufuApi,
+    TribufuApiOptions,
+    TribufuApiSingleton,
+};
