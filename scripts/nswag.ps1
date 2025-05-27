be21a2496eed1b3cdf29cea2c6f43da1d4dac1f1
Guilherme Werner <guilherme.werner@tribufu.com>
2025-05-27 08:11:22 -0300

    Generate code with openapi-generator (#5)

--- scripts/nswag.ps1
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-nswag run ./src/api/api.nswag
--- src/constants.ts
+++ src/constants.ts
@@ -3,27 +3,28 @@
 
 import packageJson from "../package.json";
 
-/**
- * The version of the Tribufu SDK.
- */
 export const TRIBUFU_VERSION: string = packageJson.version;
 
-/**
- * The default Tribufu WEB URL.
- */
-export const TRIBUFU_WEB_URL: string = "https://www.tribufu.com";
-
-/**
- * The default Tribufu API URL.
- */
-export const TRIBUFU_API_URL: string = "https://api.tribufu.com";
-
-/**
- * The default Tribufu CDN URL.
- */
-export const TRIBUFU_CDN_URL: string = "https://cdn.tribufu.com";
-
-/**
- * Tribufu copyright notice.
- */
 export const TRIBUFU_COPYRIGHT = `© ${new Date().getFullYear()} Tribufu.`;
+export const TRIBUFU_COPYRIGHT_WITH_RIGHTS = `${TRIBUFU_COPYRIGHT} All Rights Reserved.`;
+export const TRIBUFU_COPYRIGHT_WITH_YEARS = `Copyright © 2016-${new Date().getFullYear()} Tribufu`;
+
+export const TRIBUFU_API_URL: string = "https://api.tribufu.com";
+export const TRIBUFU_COMMUNITY_URL: string = "https://www.tribufu.com";
+export const TRIBUFU_DOCS_URL: string = "https://docs.tribufu.com";
+export const TRIBUFU_GIT_URL: string = "https://git.tribufu.com";
+export const TRIBUFU_MAVEN_URL: string = "https://mvn.tribufu.com";
+export const TRIBUFU_R2_URL: string = "https://r2.tribufu.com";
+export const TRIBUFU_S3_URL: string = "https://s3.tribufu.com";
+export const TRIBUFU_STATUS_URL: string = "https://status.tribufu.com";
+
+export const TRIBUFU_BLUESKY_URL: string = "https://bsky.app/profile/tribufu.com";
+export const TRIBUFU_DISCORD_URL: string = "https://www.tribufu.com/discord";
+export const TRIBUFU_GITHUB_URL: string = "https://github.com/tribufu";
+export const TRIBUFU_GITLAB_URL: string = "https://gitlab.com/tribufu";
+export const TRIBUFU_INSTAGRAM_URL: string = "https://www.instagram.com/tribufucom";
+export const TRIBUFU_LINKEDIN_URL: string = "https://www.linkedin.com/company/11747611";
+export const TRIBUFU_STEAM_URL: string = "https://steamcommunity.com/groups/tribufuservers";
+export const TRIBUFU_TIKTOK_URL: string = "https://www.tiktok.com/@tribufuservers";
+export const TRIBUFU_TWITCH_URL: string = "https://www.twitch.tv/tribufucom";
+export const TRIBUFU_YOUTUBE_URL: string = "https://www.youtube.com/@tribufuservers";
